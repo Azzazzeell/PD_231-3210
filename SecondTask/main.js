86555d592f51684c318b98d2991c3a4bca7c63e8
Azzazzeell <146361334+Azzazzeell@users.noreply.github.com>
2024-11-06 05:08:30 +0300

    Add files via upload

---
+++ SecondTask/main.js
@@ -0,0 +1,31 @@
+import View from './view.js';
+
+let tasks = [];
+let taskId = 0;
+
+const addTask = (text, priority) => {
+    const newTask = {
+        id: taskId++,
+        text: text,
+        completed: false,
+        priority: priority
+    };
+    tasks.push(newTask);
+    View.renderTask(newTask);
+    View.clearInput(priority);
+};
+
+const deleteTask = (id) => {
+    tasks = tasks.filter(task => task.id != id);
+    View.removeTask(id);
+};
+
+const completeTask = (id) => {
+    const task = tasks.find(task => task.id == id);
+    task.completed = !task.completed;
+    View.toggleCompleteTask(id, task.completed);
+};
+
+View.bindAddTask(addTask);
+View.bindDeleteTask(deleteTask);
+View.bindCompleteTask(completeTask);
